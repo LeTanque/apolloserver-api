5a60679f5aca9ddb3df35f1a4d5f11c51bff6fb7
Frank Martinez <frankie.martinez@gmail.com>
2019-11-25 14:11:50 -0800

    🚧 work in progress

---
+++ src/datasources/launch.js
@@ -0,0 +1,58 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class LaunchAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://api.spacexdata.com/v2/';
+  }
+
+
+  async getAllLaunches() {
+    const response = await this.get('launches');
+    return Array.isArray(response)
+      ? response.map(launch => this.launchReducer(launch))
+      : [];
+  }
+  
+  async getLaunchById({ launchId }) {
+    const response = await this.get('launches', { flight_number: launchId });
+    return this.launchReducer(response[0]);
+  }
+  
+  getLaunchesByIds({ launchIds }) {
+    return Promise.all(
+      launchIds.map(launchId => this.getLaunchById({ launchId })),
+    );
+  }
+
+
+  // this is the reducer to the above data sources
+  // ok so check it out. The reducer is the template. 
+  // The data sources above have something in common, the data is always
+  // going to be mapped over a similar looking object. 
+  // That object is the launchReducer. Reducers are a common
+  // pattern that saves you time and is similar to reducers you have built in redux.
+  launchReducer(launch) { 
+    return {
+      id: launch.flight_number || 0,
+      cursor: `${launch.launch_date_unix}`,
+      site: launch.launch_site && launch.launch_site.site_name,
+      mission: {
+        name: launch.mission_name,
+        missionPatchSmall: launch.links.mission_patch_small,
+        missionPatchLarge: launch.links.mission_patch,
+      },
+      rocket: {
+        id: launch.rocket.rocket_id,
+        name: launch.rocket.rocket_name,
+        type: launch.rocket.rocket_type,
+      },
+    };
+  }
+
+
+}
+
+module.exports = LaunchAPI;
+
+
